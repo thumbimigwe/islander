896d1844d5ddfd1340ba8fe503099e046aeade59
thumbimigwe <thumbimigwe@gmail.com>
2016-04-02 09:06:22 +0300

    added newsletter on the cms

--- src/newsletter/models.py
+++ src/newsletter/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class SignUp(models.Model):
     email = models.EmailField()
-    fullName = models.CharField(max_length=150, blank=True, null=True)
+    fullName = models.CharField(max_length=150, blank=False, null=True)
     timeStamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     
