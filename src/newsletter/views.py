9c56df464c613e0f89c7bd59a0b3f8e8f4164108
thumbimigwe <thumbimigwe@gmail.com>
2016-04-03 01:37:20 +0300

    context variables

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, "home.html", {})
+    title = "Welcome"
+    if request.user.is_authenticated():
+        title = "Welcome %s" %(request.user)
+# add A Form 
+    context = {
+        "template_title": title
+    }
+    return render(request, "home.html", context)
