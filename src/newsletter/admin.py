e83afe0cdf8a0605553ca0a06329e7edd1eecdbf
thumbimigwe <thumbimigwe@gmail.com>
2016-04-02 12:48:04 +0300

    added newsletter columns on the cms

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import SignUp
 
-admin.site.register(SignUp)
+class SignUpAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "fullName", "timeStamp", "updated",]
+    class Meta:
+        model = SignUp
+
+admin.site.register(SignUp, SignUpAdmin)
