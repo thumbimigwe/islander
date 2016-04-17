93b157a717b4e09bc8552b421ece901799ea51b1
thumbimigwe <thumbimigwe@gmail.com>
2016-04-17 14:50:01 +0300

    save

--- src/Newsletter/views.py
+++ src/Newsletter/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    def home(request):
+	title = 'Sign Up Now'
+	form = SignUpForm(request.POST or None)
+	context = {
+		"title": title,
+		"form": form
+	}
+	if form.is_valid():
+		#form.save()
+		#print request.POST['email'] #not recommended
+		instance = form.save(commit=False)
+
+		full_name = form.cleaned_data.get("full_name")
+		if not full_name:
+			full_name = "New full name"
+		instance.full_name = full_name
+		# if not instance.full_name:
+		# 	instance.full_name = "Justin"
+		instance.save()
+		context = {
+			"title": "Thank you"
+		}
+
+    return render(request, "home.html",{})
--- src/islander/settings.py
+++ src/islander/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Newsletter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'islander.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
